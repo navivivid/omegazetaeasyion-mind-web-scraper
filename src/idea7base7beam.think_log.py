e469aefba8fa629947a41e5bdb184115bc4953bc
navivivid <navi32946@gmail.com>
2025-04-12 14:43:08 +0900

    Update development log at 2025-04-12 14:43:08

--- src/idea7base7beam.think_log.py
+++ src/idea7base7beam.think_log.py
@@ -152,3 +152,27 @@ def bubble_sort(arr):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
+
+# Update at 2025-04-12 14:43:08
+# Fixed some bugs
+# Added documentation
+# This is a random comment
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
