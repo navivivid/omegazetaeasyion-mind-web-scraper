36d3893267e953902a69045a2ad4088c5517344c
navivivid <navi32946@gmail.com>
2025-04-15 07:47:03 +0900

    Update development log at 2025-04-15 07:47:03

--- src/idea7base7beam.think_log.py
+++ src/idea7base7beam.think_log.py
@@ -220,3 +220,14 @@ def save_to_json(data, filename):
 def load_from_json(filename):
     with open(filename, 'r') as f:
         return json.load(f)
+
+# Update at 2025-04-15 07:47:03
+# This is a random comment
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
