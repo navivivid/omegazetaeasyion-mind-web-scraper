d109e21199b5fa20cb506ce08c73fd0df5aaf4f6
navivivid <navi32946@gmail.com>
2025-04-17 15:25:05 +0900

    Update development log at 2025-04-17 15:25:04

--- src/idea7base7beam.think_log.py
+++ src/idea7base7beam.think_log.py
@@ -265,3 +265,19 @@ class LinkedList:
         while current.next:
             current = current.next
         current.next = new_node
+
+# Update at 2025-04-17 15:25:04
+# This is a random comment
+
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
