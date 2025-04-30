9bf983d156a67660a2eefcef3579cb565c55dd5b
navivivid <navi32946@gmail.com>
2025-04-30 12:28:12 +0900

    Update development log at 2025-04-30 12:28:11

--- src/idea7base7beam.think_log.py
+++ src/idea7base7beam.think_log.py
@@ -495,3 +495,17 @@ def get_date_range(start_date, end_date):
         date_list.append(current_date)
         current_date += timedelta(days=1)
     return date_list
+
+# Update at 2025-04-30 12:28:11
+# Fixed some bugs
+# Refactored the code
+# Added unit tests
+from datetime import datetime, timedelta
+
+def get_date_range(start_date, end_date):
+    date_list = []
+    current_date = start_date
+    while current_date <= end_date:
+        date_list.append(current_date)
+        current_date += timedelta(days=1)
+    return date_list
