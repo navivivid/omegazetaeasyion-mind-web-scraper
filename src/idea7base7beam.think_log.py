d071d003daddb1588ce732e24bfa51822641a762
navivivid <navi32946@gmail.com>
2025-05-18 21:42:06 +0900

    Update development log at 2025-05-18 21:42:05

--- src/idea7base7beam.think_log.py
+++ src/idea7base7beam.think_log.py
@@ -775,3 +775,16 @@ def reverse_string(s):
 def is_palindrome(s):
     s = s.lower().replace(" ", "")
     return s == s[::-1]
+
+# Update at 2025-05-18 21:42:05
+# Fixed some bugs
+
+
+import hashlib
+
+def hash_password(password):
+    salt = "random_salt"
+    return hashlib.sha256((password + salt).encode()).hexdigest()
+
+def verify_password(password, hashed):
+    return hash_password(password) == hashed
