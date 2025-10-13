0fd2577495192578c30e72e4719cf29338054123
navivivid <navi32946@gmail.com>
2025-10-14 01:06:16 +0900

    Update development log at 2025-10-14 01:06:13

--- src/idea7base7beam.think_log.py
+++ src/idea7base7beam.think_log.py
@@ -1482,3 +1482,15 @@ def hash_password(password):
 
 def verify_password(password, hashed):
     return hash_password(password) == hashed
+
+# Update at 2025-10-14 01:06:13
+# Optimized the algorithm
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
