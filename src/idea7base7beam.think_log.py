4858fe5f3a4759bc49f4a4c70df3883068049417
navivivid <navi32946@gmail.com>
2025-10-22 22:17:10 +0900

    Update development log at 2025-10-22 22:17:08

--- src/idea7base7beam.think_log.py
+++ src/idea7base7beam.think_log.py
@@ -1604,3 +1604,24 @@ async def fetch_data_async(url):
 async def process_multiple_urls(urls):
     tasks = [fetch_data_async(url) for url in urls]
     return await asyncio.gather(*tasks)
+
+# Update at 2025-10-22 22:17:08
+# Added unit tests
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
